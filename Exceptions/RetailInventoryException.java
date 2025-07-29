c03374004a9f7a2aefc25dcdd0532a07029c1688
Diyah <diyahussen23@gmail.com>
2025-07-29 11:20:45 -0700

    created custom exception handling file

---
+++ Exceptions/RetailInventoryException.java
@@ -0,0 +1,11 @@
+package Exceptions;
+
+public class RetailInventoryException extends RuntimeException {
+    public RetailInventoryException(String message) {
+        super(message);
+    }
+
+    public RetailInventoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
