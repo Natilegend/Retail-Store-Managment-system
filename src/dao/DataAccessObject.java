db381b813df8ebb3e4147e349d4abb74d19417db
Nati <justplaying@gmail.com>
2025-07-31 19:18:20 +0300

    created this interface that hold method signatures

---
+++ src/dao/DataAccessObject.java
@@ -0,0 +1,10 @@
+package dao;
+
+import Exception.RetailInventoryException;
+
+public interface DataAccessObject<T> {
+    void create(T entity) throws RetailInventoryException;
+    T findById(int id) throws RetailInventoryException;
+    void update(T entity) throws RetailInventoryException;
+    void delete(int id) throws RetailInventoryException;
+}
