74ae9d5fa65f77d6ccac4a38aae5cf54b9f7087b
Diyah <diyahussen23@gmail.com>
2025-07-31 04:44:15 -0700

    Created a java class that will represent product category

---
+++ src/model/Category.java
@@ -0,0 +1,12 @@
+package src.model;
+
+public class Category extends InventoryItem {
+    public Category(String name, int id) {
+        super(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{id=" + getId() + ", name=" + getName() + '}';
+    }
+}
